60936938cedd250c1a25346494fa61261a47b9f2
Ted Juan <ted.juan@gmail.com>
2022-04-25 19:07:43 +0800

    optimized code

--- uva10191/uva10191.cpp
+++ uva10191/uva10191.cpp
@@ -6,7 +6,7 @@
 #include <bits/stdc++.h>
 #include <boost/format.hpp>
 
-//#define ONLINE_JUDGE
+#define ONLINE_JUDGE
 #ifndef ONLINE_JUDGE    
 #include <gmock/gmock.h>
 #endif
@@ -28,12 +28,13 @@ public:
         int64_t diff = (hours - t.hours) * 60 + minutes - t.minutes;
         return Time(diff/60, diff%60);
     }
-    string show_time()
-    {
-        string text = str(boost::format("%02d:%02d") % hours % minutes);
-        return text;
-    }
-    string show_text_time()
+    friend std::ostream &operator<< (std::ostream & os, const Time &t)
+    {   
+        string text = str(boost::format("%02d:%02d") % t.hours % t.minutes);
+        os << text;
+        return os; 
+    }   
+    string print_text_time()
     {
         const string hours_text = (hours > 0) ? to_string(hours) + " hours and " : "";
         return  hours_text + to_string(minutes) + " minutes.";
@@ -43,82 +44,19 @@ private:
     int minutes;
 };
 
-class time_set
+class Time_set
 {
 public:    
-    time_set(Time _start_time, Time _end_time) 
+    Time_set(Time _start_time, Time _end_time) 
         : start_time(_start_time), end_time(_end_time)
         , interval(_end_time - _start_time) {}
 
-    bool greater_start_time(const time_set &t1)
-    {
-        return (start_time > t1.start_time);
-    }
-    bool is_get_off_early()
-    {
-        return (end_time > start_time);
-    }
-    static bool compare_interval(const time_set &n1, const time_set &n2)
-    {
-        return (n2.interval > n1.interval); 
-    }
-    static bool compare_start_time(const time_set &n1, const time_set &n2)
-    {
-        return (n2.start_time > n1.start_time); 
-    }
-    const Time get_start_time() const
-    {
-        return start_time;
-    }
-    const Time get_end_time() const
-    {
-        return end_time;
-    }
-    void set_start_time(const Time &t)
-    {
-        start_time = t;
-    }
-    const string show_start_time() 
-    {
-        return start_time.show_time();
-    }
-    const string show_interval()
-    {
-        return interval.show_text_time(); 
-    }
-private:
     Time start_time;
     Time end_time;
     Time interval;
 };
 
-time_set find_the_longest_nap_time(vector <time_set> &nap_time_list)
-{
-    auto max = *std::max_element(nap_time_list.begin(),
-                            nap_time_list.end(),
-                            time_set::compare_interval);
-    return max;                             
-}
-
-void compute_nap_time(vector <time_set> &work_time_list, vector <time_set> &nap_time_list)
-{
-    time_set tmp ({10, 0}, {18, 0});
-    nap_time_list.clear();
-
-    sort(work_time_list.begin(), work_time_list.end(),time_set::compare_start_time);
-
-    for ( auto &it : work_time_list)
-    {
-        if (it.greater_start_time(tmp))
-            nap_time_list.push_back({tmp.get_start_time(), it.get_start_time()});
-
-        tmp.set_start_time(it.get_end_time());
-    }    
-    if (tmp.is_get_off_early())
-        nap_time_list.push_back({tmp.get_start_time(), tmp.get_end_time()});
-}
-
-bool read_daily_to_work_list(std::istream & is, vector <time_set> &work_time_list)
+bool read_daily_to_work_list(std::istream & is, vector <Time_set> &work_time_list)
 {
     int num; 
     string line;
@@ -139,22 +77,61 @@ bool read_daily_to_work_list(std::istream & is, vector <time_set> &work_time_lis
             &end_hours,
             &end_minutes);
         work_time_list.push_back(
-                time_set({start_hours, start_minutes}, {end_hours, end_minutes}));
+                Time_set({start_hours, start_minutes}, {end_hours, end_minutes}));
     }        
     return true;
 }
 
-void print_the_longest_nap_time(std::ostream & os, int day, time_set &nap)
+void compute_nap_time(vector <Time_set> &work_time_list, vector <Time_set> &nap_time_list)
+{
+    Time last_work_end_time{10,0};
+    Time get_off_time{18,0};
+    nap_time_list.clear();
+
+    sort(work_time_list.begin(), 
+         work_time_list.end(),
+         [] (auto & lt, auto & rt) 
+         {
+            return (rt.start_time > lt.start_time); 
+         });
+
+    /* two case to find nap time */
+    /* case 1: current work start time > last work end time */
+    /* case 2: get off time > last work end time */
+    for ( auto &current : work_time_list)
+    {
+        if (current.start_time > last_work_end_time)
+            nap_time_list.push_back({last_work_end_time, current.start_time});
+
+        last_work_end_time = current.end_time;
+    }    
+    if (get_off_time > last_work_end_time)
+        nap_time_list.push_back({last_work_end_time, get_off_time});
+}
+
+Time_set find_the_longest_nap_time(vector <Time_set> &nap_time_list)
+{
+    auto max = *std::max_element(nap_time_list.begin(),
+                            nap_time_list.end(),
+                            [] (auto & lt, auto & rt) 
+                            {
+                                return (rt.interval > lt.interval); 
+                            });
+
+    return max;                             
+}
+
+void print_the_longest_nap_time(std::ostream & os, int day, Time_set &nap)
 {
         os << "Day #" << day << ": the longest nap starts at " << 
-            nap.show_start_time() << " and will last for " << 
-            nap.show_interval() << endl;
+            nap.start_time << " and will last for " << 
+            nap.interval.print_text_time() << endl;
 }
 
 void resolve_uva(std::istream & is, std::ostream & os)
 {
-    vector <time_set> work_time_list;
-    vector <time_set> nap_time_list;
+    vector <Time_set> work_time_list;
+    vector <Time_set> nap_time_list;
     int day = 1;
     
     while(1)
@@ -163,7 +140,7 @@ void resolve_uva(std::istream & is, std::ostream & os)
             break;
 
         compute_nap_time(work_time_list, nap_time_list);
-        time_set nap = find_the_longest_nap_time(nap_time_list);
+        Time_set nap = find_the_longest_nap_time(nap_time_list);
         print_the_longest_nap_time(os, day, nap);
         day++;            
     }
